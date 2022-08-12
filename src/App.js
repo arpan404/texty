c262c9b4d8d321b04b9f4cd53d422632c5ed3d60
arpan404 <itsmearpan1@gmail.com>
2022-08-12 22:30:15 +0545

    1.0

--- src/App.js
+++ src/App.js
@@ -1,38 +1,58 @@
-
-import { useState } from 'react';
-import './App.css';
-import NavBar from './components/Navbar';
-import TextForm from './components/TextForm ';
-import { BrowserRouter as Router,Routes, Route} from 'react-router-dom'
-
-
+import { useState } from "react";
+import "./App.css";
+import NavBar from "./components/Navbar";
+import TextForm from "./components/TextForm ";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [modes, setMode] = useState(localStorage.getItem("mode"));
+  if(modes === "dark"){
+    document.body.style.backgroundColor = "rgb(20, 20, 20)";
 
-  const [modes, setMode] = useState("light");
-
-  const toggleMode =()=>{
-    if(modes ==='light'){
-      setMode('dark');
-      document.body.style.backgroundColor = "rgb(20, 20, 20)";
-    }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor = "#f0f0f0";
-    }
   }
+  else{
+    document.body.style.backgroundColor = "#f0f0f0";
+  }
+  const toggleMode = () => {
+    if (modes === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "rgb(20, 20, 20)";
+      localStorage.clear();
+      localStorage.setItem("mode", "dark");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "#f0f0f0";
+      localStorage.clear();
+      localStorage.setItem("mode", "light");
+    }
+  };
   return (
-  <>
-  <Router>
-   <NavBar title = "TextUtils" about = "About" mode = {modes} toggleMode = {toggleMode}/>
-<Routes>
-  <Route exact path ="/home" element = {<TextForm heading= "Enter a text to analyze" mode = {modes}/>}/>
-  
-<Route index element = {<TextForm heading= "Enter a text to analyze" mode = {modes}/>}/>
-  
-   </Routes>
-   </Router>
-   </>
+    <>
+      <Router>
+        <NavBar
+          title="TextUtils"
+          about="About"
+          mode={modes}
+          toggleMode={toggleMode}
+        />
+        <Routes>
+          <Route
+            exact
+            path="/home"
+            element={
+              <TextForm heading="Enter a text to analyze" mode={modes} />
+            }
+          />
+
+          <Route
+            index
+            element={
+              <TextForm heading="Enter a text to analyze" mode={modes} />
+            }
+          />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,68 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-export default function NavBar(props) {  
+export default function NavBar(props) {
  
-    return ( 
-        <>
-        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`} >
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/home">{props.title}</Link>
-        
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <i className="nav-link active" aria-current="page">&#8594; Home</i>
-            </li>
-           
-            
-          </ul>
-          <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
-  <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheck" onClick ={props.toggleMode}/>
-  <label className="form-check-label" htmlFor="flexSwitchCheck">Dark Mode</label>
-</div>
+  return (
+    <>
+      <nav
+        className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+      >
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/home">
+            {props.title}
+          </Link>
 
-         
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <i className="nav-link active" aria-current="page">
+                  &#8594; Home
+                </i>
+              </li>
+            </ul>
+            <div
+              className={`form-check form-switch text-${
+                props.mode === "light" ? "dark" : "light"
+              }`}
+            >
+              <input
+                className="form-check-input"
+                type="checkbox"
+                role="switch"
+                id={props.mode === "dark"?"flexSwitchCheckChecked":"flexSwitchCheck"}
+                onClick={props.toggleMode}
+                
+                
+              />
+              
+              <label className="form-check-label" htmlFor={props.mode === "dark"?"flexSwitchCheckChecked":"flexSwitchCheck"}>
+                {localStorage.getItem("mode") !== "dark"?"Light" :"Dark"}
+              </label>
+            </div>
+          </div>
         </div>
-      </div>
-    </nav>
-    
-        </>
-     );
+      </nav>
+    </>
+  );
 }
 NavBar.propTypes = {
-  title : PropTypes.string.isRequired,
-  about : PropTypes.string 
-
-}
+  title: PropTypes.string.isRequired,
+  about: PropTypes.string,
+};
 NavBar.defaultProps = {
-  title: 'TextUtils',
-  mode: 'light'
-}
+  title: "TextUtils",
+  mode: "light",
+};
